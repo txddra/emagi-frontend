9e576d1af687a247af9d281b88ee051c729ef64d
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-07-09 13:17:16 -0400

    convert

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ const search = require('./emagi-search');
 const convert = require('./emagify');
 const random = require('./emagi-random');
 
+
+//arguments 
+const arguments = process.argv.slice(2)
+//if they passed in string arguments then they execute the command associated
+if(arguments[0] === 'convert'){
+    console.log(convert(arguments.slice(1)))
+}
+// console.log(arguments)
